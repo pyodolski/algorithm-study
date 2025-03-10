37844019a7bca50fb3c54c8a3bb72a9054fd57f4
pyodolski <pjwp0928w@gmail.com>
2025-03-10 12:10:31 +0900

    250310

---
+++ src/chap1/Shell.java
@@ -0,0 +1,20 @@
+package chap1;
+
+public class Shell extends AbstractSort {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h < N/3) h = 3*h + 1;
+        while (h >= 1) {
+            for (int i = h; i < N; i++)
+                for (int j = i; j >= h && less(a[j], a[j-h]); j -= h)
+                    exch(a, j, j-h);
+            h /= 3;
+        }
+    }
+    public static void main(String[] args) {
+        Integer[] a = {10, 4, 5, 2, 1, 8, 3, 6};
+        Shell.sort(a);
+        Shell.show(a);
+    }
+}
